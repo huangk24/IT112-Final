ff2e2bf92b8221c1ad662cf6b6d8179541256519
huangk24 <huangk24@uw.edu>
2022-03-25 20:11:07 -0700

    car detail

--- car/urls.py
+++ car/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('cars/', views.cars, name = 'cars')
+    path('cars/', views.cars, name = 'cars'),
+    path('carDetail/<int:id>', views.carDetail, name = 'detail'),
 ]
--- car/views.py
+++ car/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Car, CarType, Review
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,7 @@ def index(request):
 def cars(request):
     car_list = Car.objects.all()
     return render(request, 'car/cars.html', {'car_list': car_list})
+
+def carDetail(request, id):
+    car = get_object_or_404(Car, pk = id)
+    return render(request, 'car/cardetail.html', {'car': car})
