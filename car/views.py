c684c586aa58da2bf41019ce8424968efa493257
huangk24 <huangk24@uw.edu>
2022-03-25 17:44:53 -0700

    car view

--- car/views.py
+++ car/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Car, CarType, Review
 
 # Create your views here.
 def index(request):
     return render(request, 'car/index.html')
+
+def cars(request):
+    car_list = Car.objects.all()
+    return render(request, 'car/cars.html', {'car_list': car_list})
